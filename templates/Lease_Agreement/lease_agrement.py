87f42db94aae53341e53db96c7142a0b49efc53c
Devbrat Anuragi <39759685+AslanDevbrat@users.noreply.github.com>
2022-09-26 13:04:30 +0530

    Added lease agreement template
    Added template for lease agreement and added degradation folder

---
+++ templates/Lease_Agreement/lease_agrement.py
@@ -0,0 +1,33 @@
+import jinja2
+import os
+from jinja2 import Template
+import os
+import yaml
+
+with open("./lease_agreement_data.yaml") as file:
+    data = yaml.load(file , Loader = yaml.FullLoader)
+    print(data)
+
+
+latex_jinja_env = jinja2.Environment(
+	block_start_string = '\BLOCK{',
+	block_end_string = '}',
+    variable_start_string = '\VAR{',
+	variable_end_string = '}',
+    comment_start_string = '\#{',
+	comment_end_string = '}',
+	line_statement_prefix = '%%',
+	line_comment_prefix = '%#',
+    trim_blocks = True,
+	autoescape = False,
+	loader = jinja2.FileSystemLoader(os.path.abspath('.'))
+)
+
+
+template  = latex_jinja_env.get_template('./lease_agreement.tex.jinja')
+
+output = template.render(data)
+with open('./lease_agreement.tex','w') as f: 
+    f.write(output)
+
+os.system("xelatex ./lease_agreement.tex")
